91f1f5e6093bdd5a58310cda8a0cd462ac90ca5e
unknown <AvulaRudra>
2024-04-25 11:39:35 +0530

    sponsers

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,16 +33,18 @@
       let now = new Date().getTime(),
           distance = countDown - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
+     
+      if (distance < 0) {
+		document.getElementById('days').innerText = '0',
+        document.getElementById('hours').innerText = '0',
+        document.getElementById('minutes').innerText ='0',
+        document.getElementById('seconds').innerText = '0';
+      }
 
     }, second)
 
